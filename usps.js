bf1ec0f9e273428a5855344526b9280af37e7580
richmulhern <rich.mulhern@gmail.com>
2012-09-10 22:02:35 -0400

    beginning of object model. base off this.

--- usps.js
+++ usps.js
@@ -3,24 +3,74 @@
 
 var http = require('http')
   , xml2js = require('xml2js')
-  , config = require('./lib/config/config');
+  , events = require('events')
+  , config = require('./lib/config/config')
+  , util = require('util');
+
+function UspsClient() {
+
+}
+
+/* Inherits from `http.ClientRequest` */
+
+
+util.inherits( UspsClient, events.EventEmitter );
+
+UspsClient.prototype.connect = function( url, callback ) {
+
+  var uspsObj = this;
+  callback( uspsObj );
+
+  var httpReq = http.get(options, function(res) { 
+    res.on('data', function( chunk ) {
+
+      var parser = new xml2js.Parser();
+      parser.parseString(chunk, function(err, result) {
+        uspsObj.emit('data', result);
+      });
+
+    });
+  });
+
+
+/*, function(res) {
+
+
+    res.on('data', function(chunk) {
+
+      var parser;
+      parser = new xml2js.Parser();
+      return parser.parseString(chunk, function(err, result) {
+        if( result.TrackResponse ) {
+          console.log(result.TrackResponse.TrackInfo);
+        } else {
+          console.log(result);  
+        }
+      });
+    });
+
+    res.on('end', function() {
+      return console.log('done');
+    });
+
+    res.on('error', function(e) {
+      console.log(this);
+      this.emit('error', e);
+    }); 
+
+    callback(res);
+  });*/
+
+}
 
 var options = 'http://testing.shippingapis.com/ShippingAPITest.dll?API=TrackV2&XML=<TrackRequest USERID="' + config.usps.userId + '"><TrackID ID="EJ958083578US"></TrackID></TrackRequest>';
 
-var events = require("events");
+usps = new UspsClient();
 
-http.get(options, function(res) {
-  res.on('data', function(chunk) {
-    var parser;
-    parser = new xml2js.Parser();
-    return parser.parseString(chunk, function(err, result) {
-      return console.log(result.TrackResponse.TrackInfo);
-    });
-  });
-  res.on('end', function() {
-    return console.log('done');
-  });
-  return res.on('error', function(e) {
-    return console.log('problems: ' + e.message);
-  });
+usps.connect( options, function(res) {
+    console.log('CONNECTING...');
+
+    res.on('data', function(data) {
+      console.log(data);
+    })
 });
